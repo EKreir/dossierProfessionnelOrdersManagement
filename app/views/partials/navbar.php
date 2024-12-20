dd1bd1bb9881c7842d55483d9df60b74224fe78b
EKreir <ekreir97@gmail.com>
2024-12-20 15:42:22 +0100

    style css bootstrap navbar

--- app/views/partials/navbar.php
+++ app/views/partials/navbar.php
@@ -1,7 +1,24 @@
-<nav>
-    <ul>
-        <li><a href="/products">Produits</a></li>
-        <li><a href="/product/create">Ajouter un produit</a></li>
-        <li><a href="/orders">Commandes</a></li>
-    </ul>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="#">Admin Panel</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/products">Produits</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/product/create">Ajouter un produit</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/orders">Commandes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/logout">Déconnexion</a>
+                </li>
+            </ul>
+        </div>
+    </div>
 </nav>
